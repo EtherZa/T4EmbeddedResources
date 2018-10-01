491572d950c20b1fe7ba33726caf8d92f468a9c6
Richard Pringle <richard.pringle@riotinto.com>
2018-10-01 11:59:16 +0800

    Added initial code

---
+++ src/T4EmbeddedResources.Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using T4EmbeddedResources.Sample.Resources;
+
+namespace T4EmbeddedResources.Sample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = TextResources.HelloWorld.GetTemplateStream())
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    var text = streamReader.ReadToEnd();
+
+                    Console.WriteLine(text);
+                }
+            }
+        }
+    }
+}
---
+++ src/T4EmbeddedResources/install.ps1
@@ -0,0 +1,4 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$project.ProjectItems.Item("T4EmbeddedResources.tt").Properties.Item("BuildAction").Value = 0 #prjBuildActionNone
+$project.ProjectItems.Item("T4EmbeddedResources.tt.settings.xml").Properties.Item("BuildAction").Value = 0
